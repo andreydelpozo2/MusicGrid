0c6408f11e90964840ea8c71b22481615ce06882
Andrey Del Pozo <andrey.delpozo@gmail.com>
2013-05-31 00:58:17 -0500

    Add Nehe arc ball code

---
+++ MusicCube/HHCamera.h
@@ -0,0 +1,13 @@
+//
+//  HHCamera.h
+//  MusicCube
+//
+//  Created by Andrey Del Pozo on 5/31/13.
+//  Copyright (c) 2013 Andrey Del Pozo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HHCamera : NSObject
+
+@end
---
+++ MusicCube/HHCamera.m
@@ -0,0 +1,13 @@
+//
+//  HHCamera.m
+//  MusicCube
+//
+//  Created by Andrey Del Pozo on 5/31/13.
+//  Copyright (c) 2013 Andrey Del Pozo. All rights reserved.
+//
+
+#import "HHCamera.h"
+
+@implementation HHCamera
+
+@end
